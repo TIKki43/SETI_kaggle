2b0eb25028c958e77b148de752ae0fef4604e99b
TIKki43 <timur430034space@gmail.com>
2021-06-15 20:47:01 +0300

    nn's changes

--- model.py
+++ model.py
@@ -4,11 +4,11 @@ from data_preparation import *
 
 model = tf.keras.models.Sequential([
 
-    tf.keras.layers.Conv2D(32, (3, 3), input_shape=(150, 150, 3)),
+    tf.keras.layers.Conv2D(32, (6, 4), input_shape=(150, 150, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.LeakyReLU(alpha=0.3),
 
-    tf.keras.layers.Conv2D(64, (3, 3)),
+    tf.keras.layers.Conv2D(64, (6, 4)),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.LeakyReLU(alpha=0.3),
 
@@ -21,7 +21,7 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 
 history = model.fit(train_generator,
                       validation_data=test_generator,
-                      steps_per_epoch=24,
+                      steps_per_epoch=40,
                       epochs=10)
 
 model.save('model.h5')
